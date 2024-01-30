111d1482d9980033921de343c6a5fdccad5f4539
Eric Yifu Liu <46115029+ericlau1987@users.noreply.github.com>
2024-01-30 22:58:29 +1100

    update create_table

--- Data-Engineering/02_aws_datawarehouse/project/create_tables.py
+++ Data-Engineering/02_aws_datawarehouse/project/create_tables.py
@@ -10,37 +10,6 @@ def drop_tables(cur, conn):
         cur (str): cursor from connection
         conn (str): connection to database
     """    
-    drop_table_SONG_query = """
-    drop table if exists raw_song
-    """
-    drop_table_LOG_query = """
-    drop table if exists raw_log
-    """
-    drop_table_dim_users_query = """
-    drop table if exists dim_users
-    """
-    drop_table_dim_songs_query = """
-    drop table if exists dim_songs
-    """
-    drop_table_dim_artists_query = """
-    drop table if exists dim_artists
-    """
-    drop_table_dim_time_query = """
-    drop table if exists dim_time
-    """
-    drop_table_fact_songplays_query = """
-    drop table if exists fact_song_plays
-    """
-    drop_table_queries = [
-        drop_table_SONG_query,
-        drop_table_LOG_query,
-        drop_table_dim_users_query,
-        drop_table_dim_songs_query,
-        drop_table_dim_artists_query,
-        drop_table_dim_time_query,
-        drop_table_fact_songplays_query
-    ]
-
     for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
@@ -53,101 +22,6 @@ def create_tables(cur, conn):
         cur (str): cursor from connection
         conn (str): connection to database
     """  
-    create_table_SONG_query = """
-    create table if not exists raw_song (
-        song_id int not null distkey,
-        artist_id int not null,
-        title text not null,
-        artist_name text not null,
-        artist_latitude double precision,
-        artist_longitude double precision,
-        artist_location text,
-        duration double precision not null,
-        year int,
-        num_songs int
-    )
-    """
-    create_table_LOG_query = """
-    create table if not exists raw_log (
-        artist text,
-        auth text not null,
-        firstname text not null,
-        gender text not null,
-        iteminsession int,
-        lastname text,
-        length double precision,
-        level text,
-        location text,
-        method text,
-        page text,
-        registration double precision,
-        sessionid int,
-        song text,
-        status int,
-        ts bigint,
-        useragent text,
-        userid int
-    )
-    """
-    create_table_dim_users_query = """
-    create table if not exists dim_users (
-        user_id bigint not null primary key distkey,
-        first_name text not null,
-        last_name text,
-        gender text,
-        level text
-    )
-    """
-    create_table_dim_songs_query = """
-    create table if not exists dim_songs (
-        song_id bigint not null primary key distkey,
-        title text not null,
-        artist_id bigint,
-        year int,
-        duration double precision
-    )
-    """
-    create_table_dim_artists_query = """
-    create table if not exists dim_artists (
-        artist_id bigint not null primary key distkey,
-        name text not null,
-        location bigint,
-        latitude double precision,
-        longitude double precision
-    )
-    """
-    create_table_dim_time_query = """
-    create table if not exists dim_time (
-        start_time timestamp,
-        hour int not null,
-        day int,
-        week int,
-        year int,
-        weekday int
-    )
-    """
-    create_table_fact_songplays_query = """
-    create table if not exists fact_song_plays (
-        songplay_id bigint primary key sortkey,
-        start_time timestamp,
-        user_id bigint sortkey,
-        level text,
-        song_id bigint,
-        artist_id bigint,
-        session_id bigint,
-        location text,
-        useragent text
-    )
-    """
-    create_table_queries = [
-        create_table_SONG_query,
-        create_table_LOG_query,
-        create_table_dim_users_query,
-        create_table_dim_songs_query,
-        create_table_dim_artists_query,
-        create_table_dim_time_query,
-        create_table_fact_songplays_query
-    ]
     for query in create_table_queries:
         cur.execute(query)
         conn.commit()
--- Data-Engineering/02_aws_datawarehouse/project/sql_queries.py
+++ Data-Engineering/02_aws_datawarehouse/project/sql_queries.py
@@ -7,41 +7,116 @@ config.read('dwh.cfg')
 
 # DROP TABLES
 
-staging_events_table_drop = ""
-staging_songs_table_drop = ""
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+staging_events_table_drop = "drop table if exists raw_events"
+staging_songs_table_drop = "drop table if exists raw_song"
+songplay_table_drop = "drop table if exists fact_song_plays"
+user_table_drop = "drop table if exists dim_users"
+song_table_drop = "drop table if exists dim_songs"
+artist_table_drop = "drop table if exists dim_artists"
+time_table_drop = "drop table if exists dim_time"
 
 # CREATE TABLES
 
 staging_events_table_create= ("""
-""")
+    create table if not exists raw_events (
+        artist text,
+        auth text not null,
+        firstname text not null,
+        gender text not null,
+        iteminsession int,
+        lastname text,
+        length double precision,
+        level text,
+        location text,
+        method text,
+        page text,
+        registration double precision,
+        sessionid int,
+        song text,
+        status int,
+        ts bigint,
+        useragent text,
+        userid int
+    )
+    """)
 
 staging_songs_table_create = ("""
-""")
+    create table if not exists raw_song (
+        song_id int not null distkey,
+        artist_id int not null,
+        title text not null,
+        artist_name text not null,
+        artist_latitude double precision,
+        artist_longitude double precision,
+        artist_location text,
+        duration double precision not null,
+        year int,
+        num_songs int
+    )
+    """)
 
 songplay_table_create = ("""
+    create table if not exists fact_song_plays (
+            songplay_id bigint primary key distkey,
+            start_time timestamp,
+            user_id bigint sortkey,
+            level text,
+            song_id bigint,
+            artist_id bigint,
+            session_id bigint,
+            location text,
+            useragent text
+        )
 """)
 
 user_table_create = ("""
+    create table if not exists dim_users (
+            user_id bigint not null primary key distkey,
+            first_name text not null,
+            last_name text,
+            gender text,
+            level text
+        )
 """)
 
 song_table_create = ("""
+    create table if not exists dim_songs (
+            song_id bigint not null primary key distkey,
+            title text not null,
+            artist_id bigint,
+            year int,
+            duration double precision
+        )
 """)
 
 artist_table_create = ("""
+    create table if not exists dim_artists (
+            artist_id bigint not null primary key distkey,
+            name text not null,
+            location bigint,
+            latitude double precision,
+            longitude double precision
+        )
 """)
 
 time_table_create = ("""
+    create table if not exists dim_time (
+        start_time timestamp,
+        hour int not null,
+        day int,
+        week int,
+        year int,
+        weekday int
+    )
 """)
 
 # STAGING TABLES
 
 staging_events_copy = ("""
-""").format()
+copy sporting_event_ticket from 's3://udacity-dend/log_data'
+    credentials 'aws_iam_role={}'
+    gzip delimiter ';' compupdate off region 'us-west-2';
+""").format(config['ARN'])
 
 staging_songs_copy = ("""
 """).format()
